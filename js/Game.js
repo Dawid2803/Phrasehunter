329c60e994a29774ee62123bc02b4d09ecd02d6e
Dawid2803 <dawidjacoba@gmail.com>
2021-09-28 21:29:33 +0200

    struggling with the active phrase

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,51 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game{
+    constructor(){
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+
+/**
+* Creates phrases for use in game
+* @return {array} An array of phrases that could be used in the game
+* @argument(amount) stipulates the amount of phrases that needs to be created
+*/
+
+    createPhrases(){
+        const phrasesArr = [
+            new Phrase('How are you'),
+            new Phrase('Do what you gotta do'),
+            new Phrase('Let the games begin'),
+            new Phrase('I love chocolate cake'),
+            new Phrase('You can do it'),
+        ]
+
+        return phrasesArr;
+    };
+
+/**
+* Selects random phrase from phrases property
+* @return {Object} Phrase object chosen to be used
+*/
+
+getRandomPhrase() {
+    const randomPhrase =  this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    return randomPhrase;
+};
+
+/**
+* Begins game by selecting a random phrase and displaying it to user
+*/  
+startGame() {
+    document.getElementById('overlay').style.display = 'none';
+    this.activePhrase = this.getRandomPhrase();
+    this.activePhrase.addPhraseToDisplay();
+
+};
+    
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase{
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+
+/**
+* Display phrase on game board
+*/
+    addPhraseToDisplay(){
+        let phraseDisplay = document.querySelector('#phrase');
+        const randomPhrase = game.getRandomPhrase().phrase;
+        for(let i = 0; i < randomPhrase.length; i++){
+            if(randomPhrase[i] === ' '){
+                const space = `<li class="space"> </li>`;
+                phraseDisplay.innerHTML += space;
+            }else{
+                const letter = `<li class="hide letter ${randomPhrase[i]}">${randomPhrase[i]}</li>`;
+                phraseDisplay.innerHTML += letter;
+            }
+        } 
+    };
+}
